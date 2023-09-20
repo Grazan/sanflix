854cc0ee7d1e463fd9ba2a2ce841db613b4ad16e
Samuel Grazan <90648893+Grazan@users.noreply.github.com>
2023-09-20 12:35:34 -0300

    Update index.js

--- src/components/FeaturedMovie/index.js
+++ src/components/FeaturedMovie/index.js
@@ -23,15 +23,15 @@ export default ({ item }) => {
                 <div className="featured--horizontal">
                     <div className='featured--name'>{item.original_name}</div>
                         <div className='featured--info'>
-                            <div className='featured--points'>{item.vote_average}pontos</div>
+                            <div className='featured--points'>{item.vote_average} pontos</div>
                             <div className='featured--year'>{firstdate.getFullYear()}</div>
-                            <div className='featured--seasons'>{item.number_of_seasons}temporada{item.number_of_seasons !==1?'s' :''}</div>
+                            <div className='featured--seasons'>{item.number_of_seasons} temporada{item.number_of_seasons !==1?'s' :''}</div>
                             <div className='featured--description'>{description}</div>
                             <div className='featured--buttons'>
                                 < a href={``}className='featured--watchbutton'>▶ Assistir</a>
                                 < a href={``}className='featured--mylistbutton'>+ Minha Lista</a>
                             </div>
-                            <div className='featured--genres'><strong>Gêneros:</strong>{genres.join(', ')}</div>
+                            <div className='featured--genres'><strong>Gêneros: </strong>{genres.join(', ')}</div>
                     </div>
                 </div>
             </div>
